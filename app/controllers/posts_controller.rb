f5cdb7e7d6ae3e47f3fd9d8d49c5cb6b1d99ac2d
Erin Hinson <hinson.erin@gmail.com>
2015-01-02 15:03:45 -0500

    Create Edit Action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,16 +17,25 @@ class PostsController < ApplicationController
 
     if @post.save
       flash[:notice] = "Your post was created"
-      redirect_to posts_path
+      redirect_to post_path
     else
       render :new
     end
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      flash[:notice] = "The post was updated"
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
 
   private
