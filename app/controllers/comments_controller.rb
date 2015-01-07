ddbf6878bfd4d94dbefc9e3dcfc5672639fb2a9d
Erin Hinson <hinson.erin@gmail.com>
2015-01-06 19:20:47 -0500

    Display comments on posts

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,14 @@
 class CommentsController < ApplicationController
   def create
-    binding.pry
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.build(params.require(:comment).permit(:body))
+    @comment.creator = User.first
+    
+    if @comment.save
+      flash[:notice]= 'Your comment was added.'
+      redirect_to post_path(@post)
+    else
+      render 'posts/show'
+    end
   end
 end
