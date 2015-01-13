ec7dc189db9d9570350666fba34df5006191c3cd
Erin Hinson <hinson.erin@gmail.com>
2015-01-13 18:06:08 -0500

    Create sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 PostitTemplate::Application.routes.draw do
   root to: 'posts#index'
 
+  get '/login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
   
   resources :posts, except: [:destroy] do
     resources :comments, only: [:create]
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141227212852) do
+ActiveRecord::Schema.define(version: 20150113223823) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20141227212852) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "post_id"
+    t.string   "password_digest"
   end
 
 end
