8ca4e9fc3ba4f030eceac8bdccf385062c73058c
Erin Hinson <hinson.erin@gmail.com>
2015-01-13 19:01:30 -0500

    create login form, new/create session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,5 +3,23 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.where(username: params[:username]).first
+
+    user = User.find_by(username: params[:username])
+
+    if user && user.authenticate(params[:password])
+        session[:user_id] = user_id
+        flash[:notice] = "Welcome, you've logged in"
+        redirect_to root_path
+    else
+      flash[:error] = "There was something wrong."
+      redirect_to login_path
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:notice] = "You have logged out"
+    redirect_to root_path
   end
 end
