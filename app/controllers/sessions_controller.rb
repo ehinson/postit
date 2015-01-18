86d05c6aee1b0a21a17436ee1a8635fac4ad8747
Erin Hinson <hinson.erin@gmail.com>
2015-01-18 14:32:52 -0500

    Voting for posts

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     user = User.find_by(username: params[:username])
 
     if user && user.authenticate(params[:password])
-        session[:user_id] = user_id
+        session[:user_id] = user.id
         flash[:notice] = "Welcome, you've logged in"
         redirect_to root_path
     else
--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,19 @@ PostitTemplate::Application.routes.draw do
   root to: 'posts#index'
 
   get '/login', to: 'sessions#new'
-  post 'login', to: 'sessions#create'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
   
   resources :posts, except: [:destroy] do
+
+    member do
+      post 'vote'
+    end
+
     resources :comments, only: [:create]
   end
   resources :categories, only: [:new, :create, :show]
-  resources :users, except: [:show, :destroy]
+  resources :users, except: [ :destroy]
   
 
 
